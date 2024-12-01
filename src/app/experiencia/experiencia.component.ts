8ad9e5e2ae71ade6a1a3e996d5b0626508924255
Gorka29 <gorkaruizaraujo@gmail.com>
2024-12-01 13:15:38 +0100

    experiencia nuevo diseño

--- src/app/experiencia/experiencia.component.ts
+++ src/app/experiencia/experiencia.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-experiencia',
@@ -13,67 +14,80 @@ export class ExperienciaComponent {
   experiencias = [
     {
       nombreEmpresa: 'CD Santurtzi',
-      puesto: 'Fisioterapeuta',
-      descripcion: 'Primer equipo División de Honor Regional y Categorías inferiores',
+      descripcion: 'Responsabilidades: <br> Rehabilitación personalizada para jugadores lesionados, asegurando su retorno seguro a la competición. Diseño e implementación de programas de readaptación física postlesión. Evaluación biomecánica y estrategias preventivas para reducir el riesgo de lesiones comunes en el deporte de alto rendimiento. Colaboración interdisciplinar con entrenadores y médicos del equipo.',
       periodo: 'Temporada 21/22',
       imgURL: '/assets/img/cdSanturtzi.png'
     },
     {
       nombreEmpresa: 'Centro Joseba Alonso Fisioterapia Avanzada',
-      puesto: 'Fisioterapeuta',
       descripcion: 'Descripción de la experiencia',
       periodo: 'Junio 2022 - Julio 2023',
       imgURL: '/assets/img/clinicaJosebaAlonso.png'
     },
     {
       nombreEmpresa: 'ITF Getxo',
-      puesto: 'Fisioterapeuta',
       descripcion: 'Descripción de la experiencia',
       periodo: 'Julio 2023',
       imgURL: '/assets/img/itf.png'
     },
     {
       nombreEmpresa: 'CD Santurtzi',
-      puesto: 'Fisioterapeuta',
       descripcion: 'Primer equipo División de Honor Regional y Categorías inferiores',
       periodo: 'Temporada 23/24',
       imgURL: '/assets/img/cdSanturtzi.png'
     },
     {
       nombreEmpresa: 'Clínica Jon Ibañez Fisioterapia',
-      puesto: 'Fisioterapeuta',
       descripcion: 'Descripción de la experiencia',
       periodo: 'Noviembre 2023 - Julio 2024',
       imgURL: '/assets/img/clinica-jon-ibanez.png'
     },
     {
       nombreEmpresa: 'Arenas Club de Getxo',
-      puesto: 'Fisioterapeuta',
       descripcion: 'Primer equipo 2 RFEF y Juvenil División de Honor Nacional',
       periodo: 'Temporada 23/24',
       imgURL: '/assets/img/arenasClub.png'
     },
     {
       nombreEmpresa: 'ITF Getxo',
-      puesto: 'Fisioterapeuta',
       descripcion: 'Descripción de la experiencia',
       periodo: 'Julio 2024',
       imgURL: '/assets/img/itf.png'
     },
     {
       nombreEmpresa: 'Club Portugalete',
-      puesto: 'Fisioterapeuta',
       descripcion: 'Primer equipo 3 RFEF',
       periodo: 'Temporada 24/25',
       imgURL: '/assets/img/clubPortugalete.png'
     },
     {
       nombreEmpresa: 'Metropolitan Bilbao',
-      puesto: 'Fisioterapeuta',
       descripcion: 'Tratamiento de lesiones musculoesqueléticas, rehabilitación post-quirúrgica y terapia manual. Atención personalizada a pacientes y elaboración de planes de tratamiento individualizados.',
       periodo: 'Octubre 2024 - Actualidad',
       imgURL: '/assets/img/metropolitan.png'
     },
   ];
+
+  mostrarDetalles(experiencia: any) {
+    Swal.fire({
+      title: experiencia.nombreEmpresa,
+      html: `
+        <p><strong>${experiencia.periodo}</strong></p>
+        <p class="mt-4 text-gray-600 leading-relaxed">${experiencia.descripcion}</p>
+      `,
+      imageUrl: experiencia.imgURL,
+      imageWidth: 200,
+      imageAlt: experiencia.nombreEmpresa,
+      showClass: {
+        popup: 'animate__animated animate__bounceInUp'
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp'
+      },
+      customClass: {
+        popup: 'rounded-popup'
+      }
+    });
+  }
 
 }
