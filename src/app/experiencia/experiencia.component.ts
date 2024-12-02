c5a9b720b3244742726307b0604fb5400f0900fb
Gorka29 <gorkaruizaraujo@gmail.com>
2024-12-02 13:03:06 +0100

    textos experiencias

--- src/app/experiencia/experiencia.component.ts
+++ src/app/experiencia/experiencia.component.ts
@@ -17,55 +17,55 @@ export class ExperienciaComponent implements OnInit, AfterViewInit {
   experiencias = [
     {
       nombreEmpresa: 'CD Santurtzi',
-      descripcion: '- Asistencia inmediata para jugadores de categorías inferiores <br> - Rehabilitación y readaptación personalizada <br> - Colaboración interdisciplinar con cuerpos técnicos del club',
+      descripcion: '- Asistencia inmediata para jugadores de categorías inferiores. <br> - Rehabilitación y readaptación personalizada. <br> - Colaboración interdisciplinar con cuerpos técnicos del club.',
       periodo: 'Temporada 21/22',
       imgURL: '/assets/img/cdSanturtzi.png'
     },
     {
       nombreEmpresa: 'Centro Joseba Alonso Fisioterapia Avanzada',
-      descripcion: 'Descripción de la experiencia',
+      descripcion: '- Evaluación y tratamiento mediante ecografía y técnicas invasivas. <br> - Atención a pacientes privados y de mutuas. <br> -Rehabilitación de pacientes con lesiones traumáticas y postquirúrgicas.',
       periodo: 'Junio 2022 - Julio 2023',
       imgURL: '/assets/img/clinicaJosebaAlonso.png'
     },
     {
       nombreEmpresa: 'ITF Getxo',
-      descripcion: 'Descripción de la experiencia',
+      descripcion: '- Recuperación de lesiones: Asistencia inmediata para lesiones musculares y articulares durante la competición. <br> - Prevención de lesiones: Tratamientos preventivos y asesoramiento para evitar lesiones de los tenistas. <br> - Rehabilitación postpartido: Técnicas de recuperación para aliviar fatiga y optimizar el rendimiento.',
       periodo: 'Julio 2023',
       imgURL: '/assets/img/itf.png'
     },
     {
       nombreEmpresa: 'CD Santurtzi',
-      descripcion: 'Primer equipo División de Honor Regional y Categorías inferiores',
+      descripcion: '- Atención urgente a jugadores de categorías inferiores: Proporcioné asistencia rápida y eficaz a jóvenes jugadores en su desarrollo. <br> - Rehabilitación y readaptación individualizada: Implementé planes de recuperación personalizados para optimizar la reintegración deportiva. <br> - Asistencia al primer equipo y ascenso logrado: Brindé soporte físico al primer equipo, contribuyendo al ascenso a la 3ª RFEF.',
       periodo: 'Temporada 23/24',
       imgURL: '/assets/img/cdSanturtzi.png'
     },
     {
       nombreEmpresa: 'Clínica Jon Ibañez Fisioterapia',
-      descripcion: 'Descripción de la experiencia',
+      descripcion: '',
       periodo: 'Noviembre 2023 - Julio 2024',
       imgURL: '/assets/img/clinica-jon-ibanez.png'
     },
     {
       nombreEmpresa: 'Arenas Club de Getxo',
-      descripcion: 'Primer equipo 2 RFEF y Juvenil División de Honor Nacional',
+      descripcion: '',
       periodo: 'Temporada 23/24',
       imgURL: '/assets/img/arenasClub.png'
     },
     {
       nombreEmpresa: 'ITF Getxo',
-      descripcion: 'Descripción de la experiencia',
+      descripcion: '- Atención inmediata a los tenistas antes y después de la competición. <br> - Tratamientos de recuperación muscular y mejora de rendimiento. <br> - Aplicación de vendajes deportivos para garantizar la protección durante la competición.',
       periodo: 'Julio 2024',
       imgURL: '/assets/img/itf.png'
     },
     {
       nombreEmpresa: 'Club Portugalete',
-      descripcion: 'Primer equipo 3 RFEF',
+      descripcion: '',
       periodo: 'Temporada 24/25',
       imgURL: '/assets/img/clubPortugalete.png'
     },
     {
       nombreEmpresa: 'Metropolitan Bilbao',
-      descripcion: 'Tratamiento de lesiones musculoesqueléticas, rehabilitación post-quirúrgica y terapia manual. Atención personalizada a pacientes y elaboración de planes de tratamiento individualizados.',
+      descripcion: '',
       periodo: 'Octubre 2024 - Actualidad',
       imgURL: '/assets/img/metropolitan.png'
     },
@@ -121,6 +121,7 @@ export class ExperienciaComponent implements OnInit, AfterViewInit {
       customClass: {
         popup: 'rounded-popup swal-mobile'
       },
+      confirmButtonText: 'Cerrar',
     });
   }
 
