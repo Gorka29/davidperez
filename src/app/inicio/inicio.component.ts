4b679ee32e49d99de1e1ba114b1a850be47ea137
Gorka29 <gorkaruizaraujo@gmail.com>
2024-12-09 13:03:02 +0100

    inicio carrousel fotos

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,19 +1,65 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { gsap } from 'gsap';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
-export class InicioComponent implements OnInit{
+export class InicioComponent implements OnInit, AfterViewInit {
+  public currentSlide = 0;
+  public slides: HTMLElement[] = [];
+  private intervalId: any;
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // Dejamos el ngOnInit vacío
+  }
+
+  ngAfterViewInit() {
     setTimeout(() => {
-      const imagen = document.querySelector('img');
-      imagen?.classList.remove('hidden');
-    }, 100);
+      this.slides = Array.from(document.querySelectorAll('.carousel-item'));
+
+      // Aseguramos que la primera diapositiva esté activa
+      if (this.slides.length > 0) {
+        this.slides.forEach((slide, index) => {
+          if (index === 0) {
+            slide.classList.add('active');
+          } else {
+            slide.classList.remove('active');
+          }
+        });
+      }
+
+      document.querySelector('.prev')?.addEventListener('click', () => this.prevSlide());
+      document.querySelector('.next')?.addEventListener('click', () => this.nextSlide());
+
+      this.intervalId = setInterval(() => this.nextSlide(), 8000);
+    }, 0);
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  public goToSlide(index: number) {
+    this.slides[this.currentSlide].classList.remove('active');
+    this.currentSlide = index;
+    this.slides[this.currentSlide].classList.add('active');
+  }
+
+  private nextSlide() {
+    this.slides[this.currentSlide].classList.remove('active');
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+    this.slides[this.currentSlide].classList.add('active');
+  }
+
+  private prevSlide() {
+    this.slides[this.currentSlide].classList.remove('active');
+    this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+    this.slides[this.currentSlide].classList.add('active');
   }
 }
